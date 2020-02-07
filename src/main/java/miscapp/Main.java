e4a6a320893f8f82f501af653f773dcde88a0adb
Jakob Persson <jakob.persson87@hotmail.com>
2020-02-07 16:00:37 +0100

    Added Map image and connected classes

--- src/main/java/miscapp/Main.java
+++ src/main/java/miscapp/Main.java
@@ -1,15 +1,63 @@
 package miscapp;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import miscapp.gameObjects.Map;
+import miscapp.options.WindowResolution;
+
+import java.awt.*;
+import java.io.FileNotFoundException;
 
 public class Main extends Application{
     public static void main(String[] args) {
         launch(args);
     }
     
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    //Instance variables
+    private double winWidth = 1080;
+    private double winHeight = 880;
     
+    //Pane canvas = new Pane();
+    private Group root = new Group();
+    
+    //Get objects
+    private MenuBar menu = new MenuBar();
+    private WindowResolution winSettings = new WindowResolution();
+    private Map mapObj;
+    {
+        try{
+            mapObj = new Map(winHeight);
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+    } //Try catch for menu object to handle FileNotFoundException, if image cant be found
+    
+    private Scene scene = new Scene(root, winWidth, winHeight);
+    
+    //Stuff to test methods
+    private Button resize = new Button("Change window size");
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+    mapObj.imageSettings(); //Get image settings method
+    menu.menuView();
+    
+    //Add objects to scene here
+    root.getChildren().add(menu.menuGrid);
+    root.getChildren().add(mapObj.viewImg);
+    
+    stage.setResizable(false);
+    stage.setScene(scene);
+    stage.show();
+    }
+    
+    public void setResolution(){
+        //Listener to change the height of the window
+        scene.widthProperty().addListener((observableValue, oldSceneWidth, newSceneWidth) -> System.out.println("Width: " + newSceneWidth));
+    
+        //Listener to change the height of the window
+        scene.heightProperty().addListener((observableValue, oldSceneHeight, newSceneHeight) -> System.out.println("Height: " + newSceneHeight));
     }
 }
---
+++ src/main/java/miscapp/options/WindowResolution.java
@@ -0,0 +1,19 @@
+package miscapp.options;
+
+import miscapp.Main;
+
+public class WindowResolution{
+    
+    public double newWidth;
+    public double newHeight;
+    
+    public void res1920x1080(){
+        this.newWidth = 1920;
+        this.newHeight = 1080;
+    }
+    
+    public void res800x600(){
+        this.newWidth = 800;
+        this.newHeight = 600;
+    }
+}
