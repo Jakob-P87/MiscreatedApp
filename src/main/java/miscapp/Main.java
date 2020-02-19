403dec9e09c82f0b92524a90302019b1dc3ef8fe
Jakob Persson <jakob.persson87@hotmail.com>
2020-02-19 15:03:48 +0100

    Cleaned up the code, Added drag function to be able to move the image.

--- src/main/java/miscapp/Main.java
+++ src/main/java/miscapp/Main.java
@@ -3,17 +3,11 @@ package miscapp;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-import miscapp.functions.Buttons;
-import miscapp.scenes.Map;
+import miscapp.scenes.MapScene;
+
+
 
 public class Main extends Application{
     
@@ -25,71 +19,34 @@ public class Main extends Application{
     private double winWidth = 1000;
     private double winHeight = 800;
     
-    //private GridPane root = new GridPane();
     private Group root = new Group();
     private Scene scene = new Scene(root, winWidth, winHeight);
     
-    private GridPane menuLayout = new GridPane();
-    private GridPane sceneItems = new GridPane();
-    
-    private Label mapLabel = new Label();
-    private Label zoomIn = new Label();
-    private Label zoomOut = new Label();
-    
     //New objects
-    private Map map = new Map();
-    private Buttons btn = new Buttons(winWidth, winHeight);
+    private MapScene map = new MapScene();
     
     @Override
     public void start(Stage stage) {
         stage.setResizable(true);
         stage.setTitle("Miscreated");
-        
-        mapSceneSettings();
+    
+        loadImages();
         
         new AnimationTimer(){
             @Override
             public void handle(long currentTime) {
-                btn.map.setOnAction(e -> {
-                    root. getChildren().add(map.showImage);
-                    map.setPosition(winWidth - map.getSize(), 0);
-                    root.getChildren().add(zoomIn);
-                    root.getChildren().add(zoomOut);
-                });
-                map.renderImage();
-                
-                addMouseScrolling(map.showImage);
+                map.imageViewController(winWidth,winHeight);
             }
         }.start();
-    
+        
         //Add to scene
-        menuLayout.addRow(0, btn.map);
-        root.getChildren().add(btn.map);
+        root.getChildren().addAll(map.sp);
         
         stage.setScene(scene);
         stage.show();
     }
     
-    public void mapSceneSettings() {
-        mapLabel.setText("MAP");
-        mapLabel.setTextFill(Color.BLACK);
-        mapLabel.setFont(Font.font("Arial", FontWeight.EXTRA_BOLD, 26));
-        
+    public void loadImages() {
         map.setImage("file:images/miscreatedMap.png");
-        
-    }
-    
-    //Method for scrolling in and out.
-    public void addMouseScrolling(Node node) {
-        node.setOnScroll((ScrollEvent event) -> {
-            // Adjust the zoom factor as per your requirement
-            double zoomFactor = 1.1;
-            double deltaY = event.getDeltaY();
-            if (deltaY < 0){
-                zoomFactor = 2.0 - zoomFactor;
-            }
-            map.showImage.setScaleX(node.getScaleX() * zoomFactor);
-            map.showImage.setScaleY(node.getScaleY() * zoomFactor);
-        });
     }
 }
---
+++ src/main/java/miscapp/scenes/MapScene.java
@@ -0,0 +1,51 @@
+package miscapp.scenes;
+
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
+
+public class MapScene{
+    private Image image;
+    private ImageView imageView = new ImageView();
+    public ScrollPane sp = new ScrollPane(imageView);
+    
+    public void setImage(String image) {
+        this.image = new Image(image);
+    }
+    
+    private void renderImage() {
+        imageView.setImage(image);
+        imageView.setScaleX(1);
+        imageView.setScaleY(1);
+        imageView.setLayoutX(0);
+        imageView.setLayoutY(0);
+    }
+    
+    //Method to change image view.
+    public void imageViewController(double winWidth, double winHeight) {
+        renderImage();
+        sp.setContent(imageView);
+        sp.setLayoutX(winWidth - 850);
+        sp.pannableProperty().set(true);
+        sp.hbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+        sp.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+        sp.setPrefSize(winWidth - 150, winHeight);
+        
+        sp.autosize();
+        
+        /**=====Activate when needed.=====*/
+        /*imageView.setOnScroll((ScrollEvent event) -> {
+            // Adjust the zoom factor as per your requirement
+            double zoomFactor = 1.1;
+            double deltaY = event.getDeltaY();
+            
+            if(deltaY < 0) {
+                zoomFactor = 2.0 - zoomFactor;
+            }
+            
+            imageView.setScaleX(imageView.getScaleX() * zoomFactor);
+            imageView.setScaleY(imageView.getScaleY() * zoomFactor);
+        });*/
+    }
+}
